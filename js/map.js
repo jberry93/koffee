eb71c9890fbe993dd664acc71daa32e2aae6e381
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-16 13:11:34 -0700

    Removed typo

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
 function mapInit () {
   var map = new google.maps.Map(document.getElementById("koffeeMap"),{
     center: { lat: 33.6860123, lng: -117.8255414 },
-    zoom: 13
-    draggable: false;
-    keyboardShortcuts: false;
-    rotateControl: false;
-    scrollwheel: false;
-    zoomControl: false;
+    zoom: 13,
+    draggable: false,
+    keyboardShortcuts: false,
+    rotateControl: false,
+    scrollwheel: false,
+    zoomControl: false
   });
 }
