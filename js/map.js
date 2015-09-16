71a0f55d408bbf639fe2a99590debb94b67ea7b1
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-16 14:38:22 -0700

    Fixed typo in js

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ function mapInit () {
   var map = new google.maps.Map(document.getElementById("koffeeMap"),{
     center: { lat: 33.6937232, lng: -117.8055461 },
     zoom: 13,
-    draggable: false
+    draggable: false,
     keyboardShortcuts: false,
     rotateControl: false,
     scrollwheel: false,
