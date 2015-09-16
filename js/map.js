8902c05e4cee64c928bd35616c28709dcd1be5e8
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-16 14:15:41 -0700

    Testing map property 1

--- js/map.js
+++ js/map.js
@@ -2,10 +2,10 @@ function mapInit () {
   var map = new google.maps.Map(document.getElementById("koffeeMap"),{
     center: { lat: 33.6860123, lng: -117.8255414 },
     zoom: 13,
-    draggable: false,
-    keyboardShortcuts: false,
-    rotateControl: false,
-    scrollwheel: false,
-    zoomControl: false
+    draggable: false
+    // keyboardShortcuts: false,
+    // rotateControl: false,
+    // scrollwheel: false,
+    // zoomControl: false
   });
 }
