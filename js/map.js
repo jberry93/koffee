99c5eedf32938b630975f2a7cec8c381e0823693
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-16 12:34:56 -0700

    Fixed typo for properties within object

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function mapInit () {
   var map = new google.maps.Map(document.getElementById("koffeeMap"),{
-    center: { lat: 33.6860123, lng: -117.8255414 }
+    center: { lat: 33.6860123, lng: -117.8255414 },
     zoom: 5
   });
 }
