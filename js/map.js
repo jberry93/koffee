738fa6699918112d01e565237d8e6656f6c6cbb9
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-16 12:32:16 -0700

    Added correct id to js file

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 function mapInit () {
-  var map = new google.maps.Map(document.getElementById("myMap"),{
+  var map = new google.maps.Map(document.getElementById("koffeeMap"),{
     center: { lat: 33.6860123, lng: -117.8255414 }
     zoom: 5
   });
