ed2c167083622d7eb1519bc6aa561d1d13c00daf
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-18 13:48:44 -0700

    #39 previous test failed therefore, placed content back into function and commented out parse code for simplicity

--- js/map2.js
+++ js/map2.js
@@ -7,63 +7,63 @@ function mapInit () {
     rotateControl: false,                                              // rotateControl is a property of map which is assigned to a boolean value indicating if the user has rotation control
     scrollwheel: false                                                 // scrollwheel is a property of map which is assigned to a boolean value indicating if the user can zoom via scrollwheel
   });
-}
-//marker BEGIN
-var markerA = new google.maps.Marker({
-  position: {lat: 33.684317,lng: -117.885642},
-  map: map,
-  label: "A"
-});
-var positionA = {lat: 33.684317,lng: -117.885642};
-var markerB = new google.maps.Marker({
-  position: {lat: 33.674104,lng: -117.770783},
-  map: map,
-  label: "B"
-});
-var positionB = {lat: 33.674104,lng: -117.770783};
-var markerC = new google.maps.Marker({
-  position: {lat: 33.649201,lng: -117.839658},
-  map: map,
-  label: "C"
-});
-var positionC = {lat: 33.649201,lng: -117.839658};
-var markerD = new google.maps.Marker({
-  position: {lat: 33.683982,lng: -117.836618},
-  map: map,
-  label: "D"
-});
-var positionD = {lat: 33.683982,lng: -117.836618};
-//marker END
-alert("Markers have been added!");
-var matrixService = new google.maps.DistanceMatrixService();
-matrixService.getDistanceMatrix(
-  {
-    origin: {lat: 33.668131,lng: -117.863560},
-    destinations: [positionA,positionB,positionC,positionD],
-    travelMode: google.maps.TravelMode.DRIVING,
-    avoidTools: true,
-    avoidFerries: true
-  }, callback
-);
-function callback (response,status) {
-  //Parse results
-  if (status === google.maps.DistanceMatrixStatus.OK) {
-    alert("Request was a success!");
-    var origins = response.originAddresses;
-    var destinations = response.destinationAddresses;
-    for (var i = 0; i < origins.length; i++) {
-      var results = response.rows[i].elements;
-      for (var j = 0; j < results.length; j++) {
-        var element = results[j];
-        var distance = element.distance.text;
-        var duration = element.duration.text;
-        var from = origins[i];
-        var to = destinations[j];
+  //marker BEGIN
+  var markerA = new google.maps.Marker({
+    position: {lat: 33.684317,lng: -117.885642},
+    map: map,
+    label: "A"
+  });
+  var positionA = {lat: 33.684317,lng: -117.885642};
+  var markerB = new google.maps.Marker({
+    position: {lat: 33.674104,lng: -117.770783},
+    map: map,
+    label: "B"
+  });
+  var positionB = {lat: 33.674104,lng: -117.770783};
+  var markerC = new google.maps.Marker({
+    position: {lat: 33.649201,lng: -117.839658},
+    map: map,
+    label: "C"
+  });
+  var positionC = {lat: 33.649201,lng: -117.839658};
+  var markerD = new google.maps.Marker({
+    position: {lat: 33.683982,lng: -117.836618},
+    map: map,
+    label: "D"
+  });
+  var positionD = {lat: 33.683982,lng: -117.836618};
+  //marker END
+  alert("Markers have been added!");
+  var matrixService = new google.maps.DistanceMatrixService();
+  matrixService.getDistanceMatrix(
+    {
+      origin: {lat: 33.668131,lng: -117.863560},
+      destinations: [positionA,positionB,positionC,positionD],
+      travelMode: google.maps.TravelMode.DRIVING,
+      avoidTools: true,
+      avoidFerries: true
+    }, callback
+  );
+  function callback (response,status) {
+    //Parse results
+    if (status === google.maps.DistanceMatrixStatus.OK) {
+      alert("Request was a success!");
+      // var origins = response.originAddresses;
+      // var destinations = response.destinationAddresses;
+      // for (var i = 0; i < origins.length; i++) {
+      //   var results = response.rows[i].elements;
+      //   for (var j = 0; j < results.length; j++) {
+      //     var element = results[j];
+      //     var distance = element.distance.text;
+      //     var duration = element.duration.text;
+      //     var from = origins[i];
+      //     var to = destinations[j];
+        }
       }
+    } else if (status === google.maps.DistanceMatrixStatus.REQUEST_DENIED) {
+      alert("Request has been denied");
+    } else if (status === google.maps.DistanceMatrixStatus.UNKNOWN_ERROR) {
+      alert("Request was not processed due to server error");
     }
-  } else if (status === google.maps.DistanceMatrixStatus.REQUEST_DENIED) {
-    alert("Request has been denied");
-  } else if (status === google.maps.DistanceMatrixStatus.UNKNOWN_ERROR) {
-    alert("Request was not processed due to server error");
   }
 }
