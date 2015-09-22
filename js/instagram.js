43156b96af0ee7d9bbdd27dd89e56cbb6a45e1d6
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-22 14:06:33 -0700

    #41 changed class attribute to id and concatenated the list of pictures into the coffeePics div

--- js/instagram.js
+++ js/instagram.js
@@ -1,8 +1,6 @@
 function callbackFunction(data) {
-  var newContent = "";
   var coffeePics = document.getElementById("coffeePics");
   for(var pic = 0; pic < 8; pic++) {
-    newContent += "<li><a target='_blank' href='" + data.data[pic].link + "'><img src='" + data.data[pic].images.low_resolution.url + "'></img></a></li>";
+    coffeePics.innerHTML += "<li><a href='" + data.data[pic].link + "'><img src='" + data.data[pic].images.low_resolution.url + "'></img></a></li>";
   }
-  document.write(newContent);
 }
