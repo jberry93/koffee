80c0d0394975d0163be4ad17cdcfcea87e4d70c1
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-23 11:20:50 -0700

    #40 changed event and added more to function, bug is present

--- js/orderCalculator.js
+++ js/orderCalculator.js
@@ -8,13 +8,61 @@ var mokaccinoDrinkNumber = document.getElementById("mokaccinoDrop");
 var capheDrinkNumber = document.getElementById("capheDrop");
 var chakoDrinkNumber = document.getElementById("chakoDrop");
 var fernDrinkNumber = document.getElementById("fernDrop");
-function getTokyoDrinks() {
-  var tokyoCostDiv = document.getElementById("tokyoCost");
-  if(tokyoDrinkNumber.value > 0) {
-    tokyoCostDiv.setAttribute("class", "show");
-    tokyoCostDiv.innerHTML = "<p>Tokyo Latte: 3.75 X " + tokyoDrinkNumber.value + "</p>";
+var submitButton = document.getElementById("firstSubmit");
+
+function getAllDrinks(lowerCaseName, drinkNumber, upperCaseName, price) {
+  var costDiv = document.getElementById(lowerCaseName + "Cost");
+  var feesCostDiv = document.getElementById("fees");
+  var totalCostDiv = document.getElementById("totalCost");
+  if(drinkNumber.value > 0) {
+    var salesTax = 0.08;
+    var deliveryFee = 3;
+    var grandTotal = 0;
+    var subTotal = 0;
+    var tax = 0;
+    subTotal = drinkNumber.value * price;
+    tax = subTotal * salesTax;
+    grandTotal = subTotal + tax + deliveryFee;
+    costDiv.setAttribute("class", "show");
+    feesCostDiv.setAttribute("class", "show");
+    totalCostDiv.setAttribute("class", "show");
+    costDiv.innerHTML = "<p>" + upperCaseName + ": " + price + " X " + drinkNumber.value + "</p>";
+    feesCostDiv.innerHTML = "<p>" + "Sales Tax: " + tax.toFixed(2) + "</p>" +"<p>Delivery Fee: 3.00</p>";
+    totalCostDiv.innerHTML = "<p>" + "Total: " + grandTotal.toFixed(2) + "</p>";
   } else {
-    tokyoCostDiv.setAttribute("class", "hide");
+    costDiv.setAttribute("class", "hide");
+    feesCostDiv.setAttribute("class", "hide");
+    totalCostDiv.setAttribute("class", "hide");
   }
 }
-tokyoDrinkNumber.addEventListener("blur", getTokyoDrinks, false);
+
+tokyoDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("tokyo", tokyoDrinkNumber, "Tokyo Latte", 3.75);
+}, false);
+americanDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("american", americanDrinkNumber, "The American", 3.00);
+}, false);
+snowDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("snow", snowDrinkNumber, "Jon Snow", 3.50);
+}, false);
+bagDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("bag", bagDrinkNumber, "Bean Bag", 2.00);
+}, false);
+edenDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("eden", edenDrinkNumber, "Apple of Eden", 2.00);
+}, false);
+mokaccinoDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("mokaccino", mokaccinoDrinkNumber, "Mokaccino", 4.00);
+}, false);
+cloverDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("clover", cloverDrinkNumber, "4 Leaf Clover", 5.50);
+}, false);
+capheDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("caphe", capheDrinkNumber, "Càphê", 4.50);
+}, false);
+chakoDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("chako", chakoDrinkNumber, "Chako", 3.00);
+}, false);
+fernDrinkNumber.addEventListener("change", function() {
+  getAllDrinks("fern", fernDrinkNumber, "Red Fern", 3.00);
+}, false);
