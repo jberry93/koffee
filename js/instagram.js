fe02a8ac7715199e4391a5c673024797a12151c9
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-24 13:26:38 -0700

    Attempted refactoring js code

--- js/instagram.js
+++ js/instagram.js
@@ -1,6 +1,10 @@
-function callbackFunction(data) {
+function callbackFunction(instaData) {
   var coffeePics = document.getElementById("coffeePics");
   for(var pic = 0; pic < 8; pic++) {
-    coffeePics.innerHTML += "<li><a href='" + data.data[pic].link + "'><img src='" + data.data[pic].images.low_resolution.url + "'></img></a></li>";
+    coffeePics.innerHTML += "<li><a href='" +
+                            instaData.data[pic].link +
+                            "'><img src='" +
+                            instaData.data[pic].images.low_resolution.url + 
+                            "'></img></a></li>";
   }
 }
--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
 function mapInit () {
-  // initialize Google Maps BEGIN
   var mapContainer = document.getElementById("koffeeMap");
   var mapOptionsObject = {
     center: {lat: 33.6937232, lng: -117.8055461},                      // center is a property of map which is assigned to an object with two keys: lat and lng and assigns them numeric values. This will initialize a focal point for the map
@@ -9,8 +8,7 @@ function mapInit () {
     scrollwheel: false                                                 // scrollwheel is a property of map which is assigned to a boolean value indicating if the user can zoom via scrollwheel
   }
   var map = new google.maps.Map(mapContainer, mapOptionsObject);
-  // Initialize Google Maps END
-  // Add 4 Markers BEGIN
+
   var markerA = new google.maps.Marker({         // Call method Marker which is assigned to an object and key value pairs indicating the information for the new marker and return the marker to new variable markerA
     position: {lat: 33.684317,lng: -117.885642}, // position is a property of markerA which is assigned to an object with 2 keys lat and lng and assigns them numeric values indicating the exact position of marker placement
     map: map,                                    // map is a property of markerA which is assigned to object map indicating which map to assign this marker to
@@ -31,8 +29,7 @@ function mapInit () {
     map: map,
     label: "D"
   });
-  // Add 4 Markers END
-  // Marker Positions BEGIN
+
   var positionA = {
     lat: 33.684317,
     lng: -117.885642
@@ -49,14 +46,13 @@ function mapInit () {
     lat: 33.683982,
     lng: -117.836618
   }
-  // Marker Positions END
-  // HTML5 Geolocation API usage BEGIN
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showLocation);
   } else {
-    // no geolocation support:
     alert("Please enable geolocation");
   }
+
   function showLocation(position) {
     var myPosition = {
       lat: position.coords.latitude,
@@ -67,27 +63,40 @@ function mapInit () {
       map: map,
       label: "U"
     };
+
     var myMarker = new google.maps.Marker(myMarkerOptions);
     map.setCenter(myPosition);
-    // Distance Calculations BEGIN
+
     function radians(degrees) {
       var convert = (degrees * Math.PI) / 180;
       return convert;
     }
+
     function haversine(markerPosition) {
       var latDifference = radians(markerPosition.lat - position.coords.latitude);
       var lngDifference = radians(markerPosition.lng - position.coords.longitude);
       var radius = 3961;
-      var a = Math.pow(Math.sin(latDifference / 2), 2) + Math.cos(radians(position.coords.latitude)) * Math.cos(radians(markerPosition.lat)) * Math.pow(Math.sin(lngDifference / 2), 2);
+      var a = Math.pow(Math.sin(latDifference / 2), 2) +
+              Math.cos(radians(position.coords.latitude)) *
+              Math.cos(radians(markerPosition.lat)) *
+              Math.pow(Math.sin(lngDifference / 2), 2);
       var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       var d = radius * c;
       return d;
     }
-    // Distance Calculations END
-    // Display Results BEGIN
+
     var outputDiv = document.getElementById("distances");
-    outputDiv.innerHTML = "<p>Truck A is " + haversine(positionA).toFixed(2) + " miles away!" + "</p>" + "<p>Truck B is " + haversine(positionB).toFixed(2) + " miles away!" + "</p>" + "<p>Truck C is " + haversine(positionC).toFixed(2) + " miles away!" + "</p>" + "<p>Truck D is " + haversine(positionD).toFixed(2) + " miles away!" + "</p>";
-    // Display Results END
+    outputDiv.innerHTML = "<p>Truck A is " +
+                          haversine(positionA).toFixed(2) +
+                          " miles away!" + "</p>" +
+                          "<p>Truck B is " +
+                          haversine(positionB).toFixed(2) +
+                          " miles away!" + "</p>" +
+                          "<p>Truck C is " +
+                          haversine(positionC).toFixed(2) +
+                          " miles away!" + "</p>" +
+                          "<p>Truck D is " +
+                          haversine(positionD).toFixed(2) +
+                          " miles away!" + "</p>";
   }
-  // HTML5 Geolocation API usage END
 }
