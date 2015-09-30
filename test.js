4da0b7785a04f0b74d7883f54de497031c3a4f1a
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-30 14:39:55 -0700

    Added a second suite of tests to check if route /404 sends a status of 404

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
 var express = require("express"),
-    request = require("supertest");
+    request = require("supertest"),
+    app = express();
 
-describe("Express Backend Test", function() {
-  var app = express();
-  app.get("/", function(request, response) {
-    response.sendStatus(200);
-  });
+app.get("/200", function(request, response) {
+  response.sendStatus(200);
+});
 
+app.get("/404", function(request, response) {
+  response.sendStatus(404);
+});
+
+describe("Express Backend Test 1", function() {
   it("status code 200", function(done) {
     request.agent(app)
-      .get("/")
+      .get("/200")
       .expect(200)
+      .end(done);
+  });
+});
+
+describe("Express Backend Test 2", function() {
+  it("status code 404", function(done) {
+    request.agent(app)
+      .get("/404")
+      .expect(404)
       .end(done);
   });
 });
