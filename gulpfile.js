31b19a8ae85cb5a619b153c7608977967feea112
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-09-30 16:26:20 -0700

    Removed gulp-nodemon from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,5 @@
 var gulp = require("gulp"),
-    nodemon = require("gulp-nodemon"),
     mocha = require("gulp-mocha");
-
-gulp.task("nodemon", function() {
-  nodemon({
-    script: "app.js",
-    ext: "js"
-  })
-    .on("start", function() {
-      console.log("Started express app!");
-    })
-    .on("restart", function() {
-      console.log("Restarted express app!");
-    });
-});
 
 gulp.task("test", function() {
   return gulp.src("test.js", { read: false })
@@ -24,4 +10,4 @@ gulp.task("watch", function() {
   gulp.watch("*.js", ["test"]);
 });
 
-gulp.task("default", ["nodemon", "watch", "test"]);
+gulp.task("default", ["watch", "test"]);
