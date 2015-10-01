0b4d4f5f0cb83739d5d41517776d35ba1b66a768
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-01 11:11:29 -0700

    Refined tests, cleaned up gulpfile, utilized supertest to removed routes from test.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require("gulp"),
+    nodemon = require("gulp-nodemon"),
     mocha = require("gulp-mocha");
 
+gulp.task("nodemon", function() {
+  nodemon({
+    script: "app.js",
+    ext: "js"
+  })
+    .on("start", ["test"])
+    .on("restart", function() {
+      console.log("Restarted nodemon!");
+    });
+});
+
 gulp.task("test", function() {
-  return gulp.src("test.js", { read: false })
-    .pipe(mocha({ reporter: "landing" }));
+  return gulp.src("test.js")
+    .pipe(mocha());
 });
 
 gulp.task("watch", function() {
   gulp.watch("*.js", ["test"]);
 });
 
-gulp.task("default", ["watch", "test"]);
+gulp.task("default", ["nodemon", "watch"]);
--- test.js
+++ test.js
@@ -1,65 +1,43 @@
-var express = require("express"),
-    request = require("supertest"),
-    path = require("path"),
-    app = express();
-
-app.get("/", function(request, response) {
-  response.sendFile(path.join(__dirname + "/public/html/index.html"));
-});
-app.get("/order", function(request, response) {
-  response.sendFile(path.join(__dirname + "/public/html/order.html"));
-});
-app.get("/map", function(request, response) {
-  response.sendFile(path.join(__dirname + "/public/html/map.html"));
-});
-app.get("/how-it-works", function(request, response) {
-  response.sendFile(path.join(__dirname + "/public/html/howitworks.html"));
-});
-app.get("/menu", function(request, response) {
-  response.sendFile(path.join(__dirname + "/public/html/menu.html"));
-});
-app.get("/careers", function(request, response) {
-  response.sendFile(path.join(__dirname + "/public/html/careers.html"));
-});
+var request = require("supertest")("http://localhost:8080");
 
 describe("Status Code Test", function() {
   it("status code /", function(done) {
-    request.agent(app)
+    request
       .get("/")
       .expect(200)
       .end(done);
   });
 
   it("status code /order", function(done) {
-    request.agent(app)
+    request
       .get("/order")
       .expect(200)
       .end(done);
   });
 
   it("status code /map", function(done) {
-    request.agent(app)
+    request
       .get("/map")
       .expect(200)
       .end(done);
   });
 
   it("status code /how-it-works", function(done) {
-    request.agent(app)
+    request
       .get("/how-it-works")
       .expect(200)
       .end(done);
   });
 
   it("status code /menu", function(done) {
-    request.agent(app)
+    request
       .get("/menu")
       .expect(200)
       .end(done);
   });
 
   it("status code /careers", function(done) {
-    request.agent(app)
+    request
       .get("/careers")
       .expect(200)
       .end(done);
