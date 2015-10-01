db3f9bdc92152d4f0edf814ca9ddb368c9e2a6b9
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-01 14:04:45 -0700

    Minified html and css, copied into 'dist' directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require("gulp"),
+    minHTML = require("gulp-minify-html"),
+    minCSS = require("gulp-minify-css"),
     uglify = require("gulp-uglify");
 
 gulp.task("uglify", function() {
   return gulp.src("./js/*.js")
     .pipe(uglify())
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("minHTML", function() {
+  return gulp.src("./html/*.html")
+    .pipe(minHTML())
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("minCSS", function() {
+  return gulp.src("./css/*.css")
+    .pipe(minCSS())
     .pipe(gulp.dest("dist/"));
 });
