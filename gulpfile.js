1dd656fab2c5a8a99de2db85711cf439245cf7fd
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-01 13:52:04 -0700

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp"),
     uglify = require("gulp-uglify");
 
 gulp.task("uglify", function() {
-  return gulp.src("public/js/*.js")
+  return gulp.src("./js/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest("public/dist/"));
+    .pipe(gulp.dest("dist/"));
 });
